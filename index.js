843c5bfe5a2949f9baee857108b4e4ed58552532
Tapish Verma <tapishverma176@gmail.com>
2023-07-03 14:28:04 +0530

    set up view engine

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const exp = require("constants");
+const path = require("path");
 const express = require("express");
 const port = 8000; // on production level port No- 80 is used.
 
 const app = express();
+
+app.set("views engine", "ejs");
+app.set("views",path.join(__dirname,"/views"));
+
 
 // adding express.router . This is a middleware which starts before the start of server!!
 
