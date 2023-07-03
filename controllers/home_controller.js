e02ddbad0fd41f75b46118991a0ae17f09c72c95
Tapish Verma <tapishverma176@gmail.com>
2023-07-03 14:51:31 +0530

    add views for home and made a correction in index file

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,5 @@
 module.exports.home = function(req,res){
-    return res.end("<h1>Server is up for getTogether App!! </h1>")
+    return res.render("home",{
+        title : "getTogether"
+    });
 };
--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const port = 8000; // on production level port No- 80 is used.
 
 const app = express();
 
-app.set("views engine", "ejs");
+// setting views engine and views folder;
+app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"/views"));
+
+//setting statics folder;
+
+app.use(express.static("statics"));
 
 
 // adding express.router . This is a middleware which starts before the start of server!!
