61747e73829f0edfc74e405cbccd85c54f6181ce
Tapish Verma <tapishverma176@gmail.com>
2023-07-24 13:34:10 +0530

    make controller functions for sign in and sign up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,49 @@
+const User = require("../models/user");
+
 module.exports.profile = function (req,res){
     return res.end("<h1>Users Profile Page!!</h1>")
+};
+
+module.exports.create = function(req,res){
+    if(req.body.password != req.body.ConfirmPassword){
+        return res.redirect("back");
+    }
+
+    User.findOne({email : req.body.email}).then(
+        (user) => {
+            if(!user){
+                User.create({
+                    name : req.body.name,
+                    email : req.body.email,
+                    password : req.body.password
+                }).then((createdUser) => {
+                    console.log("User Created!!!");
+                    console.log(createdUser);
+                    return res.redirect("back");
+                }).catch((error) => {
+                    console.log("Error Occurred : ",error);
+                })
+            }else{
+                console.log("Username already exists!!");
+                return res.redirect("back");
+            }
+        }
+    ).catch((err) => {
+        console.log("Error Occured", err);
+        return res.redirect("back");
+    })
+
+   
+};
+
+module.exports.signUp = function(req,res){
+    return res.render("signup.ejs",{
+        title : "getTogether | SignUp"
+    });
+};
+
+module.exports.signIn = function(req,res){
+    return res.render("signin.ejs",{
+        title : "getTogether | SignIn"
+    });
 };
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 8000; // on production level port No- 80 is used.
 
 const app = express();
 const db = require("./config/mongoose");
+const cookieParser = require("cookie-parser");
 
 // setting views engine and views folder;
 app.set("view engine", "ejs");
@@ -13,6 +14,14 @@ app.set("views",path.join(__dirname,"/views"));
 //setting statics folder;
 
 app.use(express.static("statics"));
+
+//add urlencoder middleware to app
+
+app.use(express.urlencoded({extended : true}));
+
+// add cookie-parser to the app;
+
+app.use(cookieParser());
 
 
 // adding express.router . This is a middleware which starts before the start of server!!
