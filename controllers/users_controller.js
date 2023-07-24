72cc11aa2861979cc1d720c33221aab2b388b70a
Tapish Verma <tapishverma176@gmail.com>
2023-07-24 19:09:23 +0530

    add mannual authentication to the website

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,24 @@
 const User = require("../models/user");
 
 module.exports.profile = function (req,res){
-    return res.end("<h1>Users Profile Page!!</h1>")
+    console.log(req.cookies.user_id);
+    if(req.cookies.user_id != undefined){
+        User.findOne({_id : req.cookies.user_id}).then((user) => {
+            if(!user){
+                return res.redirect("/users/sign-in");
+            }else{
+                return res.render("userprofile.ejs",{
+                    title : "getTogether | My Profile",
+                    name : user.name
+                })
+            }
+        }).catch((err) => {
+            console.log("Error occurred : ", err);
+        })
+    }else{
+        return res.redirect("/users/sign-in"); 
+    }
+    
 };
 
 module.exports.create = function(req,res){
@@ -36,6 +53,30 @@ module.exports.create = function(req,res){
    
 };
 
+module.exports.mannualAuthentication = function(req,res){
+    User.findOne({email : req.body.email}).then((user) => {
+        if(!user){
+            console.log("User not found!");
+            return res.redirect("back");
+        }else{
+            console.log(user);
+            if(user["password"] == req.body.password){
+                console.log("Signin Successful!");
+                res.cookie("user_id",user.id);
+                return res.redirect("/users/profile");
+
+            }else{
+                console.log("Incorrect Password");
+                return res.redirect("back");
+            }
+            
+        }
+    }).catch((err) => {
+        console.log("Error Occured while Signing In : ",err);
+        return res.redirect("back");
+    })
+}
+
 module.exports.signUp = function(req,res){
     return res.render("signup.ejs",{
         title : "getTogether | SignUp"
@@ -47,3 +88,10 @@ module.exports.signIn = function(req,res){
         title : "getTogether | SignIn"
     });
 };
+
+module.exports.signOut = function(req,res){
+    // delete req.cookies.user_id;
+    // res.cookie("user_id",undefined);
+    res.cookie('user_id', '', { expires: new Date(0) });
+    return res.redirect("/users/sign-in");
+}
--- routes/user.js
+++ routes/user.js
@@ -8,6 +8,8 @@ router.get("/profile",userController.profile);
 router.get("/sign-up",userController.signUp)
 router.get("/sign-in",userController.signIn)
 router.post("/create",userController.create)
+router.post("/authenticate",userController.mannualAuthentication)
+router.get("/sign-out",userController.signOut)
 
 
 module.exports = router;
