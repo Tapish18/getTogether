c3988419eec8840fca87b0d870a5e618db3f4552
Tapish Verma <tapishverma176@gmail.com>
2023-07-28 19:13:05 +0530

    add action to save post to DB and make the corresponding route

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,3 +1,28 @@
+const { create } = require("connect-mongo");
+const Post = require("../models/post");
+const User = require("../models/user");
+
+
 module.exports.myposts = function(req,res){
     return res.end("<h1>These are your posts!!</h1>");
 };
+
+
+module.exports.createPost = function(req,res){
+    User.findOne({_id : req.user._id}).then((user) => {
+        console.log(user.name);
+        Post.create({
+            content : req.body.content,
+            user : user._id
+        }).then((createdpost) => {
+            console.log("Post added Sucessfully!");
+            console.log(createdpost);
+        }).catch((error) => {
+            console.log("Error Occurred : ", error);
+        })
+    }).catch((error) => {
+        console.log("Error in Finding the User!!")
+        console.log(error);
+    })
+    return res.redirect("back");
+}
--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,11 @@ const express = require("express");
 const postController = require("../controllers/posts_controller");
 const router = express.Router();
 
+const passport = require("passport");
+
 
 router.get("/myposts", postController.myposts);
+router.post("/createpost",postController.createPost);
 
 module.exports = router;
 
